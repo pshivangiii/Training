37b6610a2979fc1511a531743be20952d4a092e4
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-02-20 17:25:01 +0530

    Add files via upload

---
+++ rps.php
@@ -0,0 +1,59 @@
+<?php
+
+function computer_choice_generation()
+{
+    $computer = rand(1,3);
+    if ($computer == 1) {
+        $computer_choice = "rock";
+        return $computer_choice;
+    }    
+    elseif ($computer == 2) {
+        $computer_choice = "paper";
+        return $computer_choice;
+    } 
+    else {
+        $computer_choice = "scissors";
+        return $computer_choice;
+    }
+}
+
+function finalize_result($player_choice, $computer_choice){
+   
+    if ($player_choice == $computer_choice) {
+        return 0;
+    } elseif ($player_choice == "rock" && $computer_choice == "scissors") {
+        return 1;
+    } elseif ($player_choice == "scissors" && $computer_choice == "paper") {
+        return 1;
+    } elseif ($player_choice == "paper" && $computer_choice == "rock") {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+function display_results($message, $player_choice, $computer_choice){
+    $file = file_get_contents("result.html");
+    $file = str_replace('{MESSAGE}', $message, $file);
+    
+    $file = str_replace('{CHOICE1}', $player_choice, $file);
+    $file = str_replace('{CHOICE2}', $computer_choice, $file);
+    echo $file;
+}
+
+if(isset($_POST["choice"])){
+    $player_choice = $_POST["choice"];    
+    $computer_choice = computer_choice_generation();
+    $message = "";
+  
+    $result = finalize_result($player_choice, $computer_choice);
+    
+    if ($result == 0) {
+        $message = "TIE";
+    } elseif ($result == 1) {
+        $message = "You won!";
+    } else {
+        $message = "Computer wins!";
+    }
+    display_results($message, $player_choice, $computer_choice);
+}
