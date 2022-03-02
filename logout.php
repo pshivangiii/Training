38f653ff64459c61fa2e8eedacf15fb8a603ace3
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-03-02 11:19:36 +0530

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,24 @@
+<?php
+   session_start();
+   include('conn.php');  
+    
+    $username = $_SESSION['uname'];  
+    $one=1;
+    $zero=0;
+        
+        $username = stripcslashes($username);  
+        $username = mysqli_real_escape_string($conn, $username);  
+       
+            $s="UPDATE user_information SET isonline='$zero' WHERE name='$username'";
+            $r = mysqli_query($conn, $s); 
+            if($r)
+            {
+                setcookie("user", "", time() - 3600); 
+                header("Location: login.html");
+            }
+            session_unset(); 
+            session_destroy();
+            header("Location: login.html");
+            exit;
+            
+    ?>
