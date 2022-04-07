4c33359fc3549464cafcb6bf112150ac4b093d25
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-04-07 16:47:54 +0530

    Add files via upload

--- Models/AdminDetails.php
+++ Models/AdminDetails.php
@@ -9,25 +9,21 @@ class AdminDetails extends Model
     protected $table="admin_details";
     protected $primaryKey="id";
 
-    public static function adminregistrationModel($email,$password){
+    public static function adminregistrationModel($email,$password)
+    {
         $reg=new AdminDetails();
         $reg->email=$email;
         $reg->password=$password;
         $reg->save();
     }
 
-    public static function login($email,$password){
+    public static function login($email,$password)
+    {
         $check=AdminDetails::where(['email'=>$email,'password'=>$password])->get();
-
         if(count($check)>0)
-
         {
-
             $users=AdminDetails::where(['email'=>$email,'password'=>$password])->get();
-            // $users=$email;
-
             $users=compact('users');
-
             return $users;
         }
 
