a6ebd7c07ab6c4637273389ee1c19b37d662d1c8
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-04-07 16:45:38 +0530

    Add files via upload

---
+++ Models/AdminDetails.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AdminDetails extends Model
+{
+    protected $table="admin_details";
+    protected $primaryKey="id";
+
+    public static function adminregistrationModel($email,$password){
+        $reg=new AdminDetails();
+        $reg->email=$email;
+        $reg->password=$password;
+        $reg->save();
+    }
+
+    public static function login($email,$password){
+        $check=AdminDetails::where(['email'=>$email,'password'=>$password])->get();
+
+        if(count($check)>0)
+
+        {
+
+            $users=AdminDetails::where(['email'=>$email,'password'=>$password])->get();
+            // $users=$email;
+
+            $users=compact('users');
+
+            return $users;
+        }
+
+    }
+}
