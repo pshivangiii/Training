92a4fb7ee6cd071f2ccdfbb050e5bdcf70269e87
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-04-14 12:52:37 +0530

    Add files via upload

--- views/newreg.blade.php
+++ views/newreg.blade.php
@@ -70,19 +70,11 @@
               echo $message;
              }
              @endphp
-            
             </div>
           <br>
           <button type="submit" class="registerbtn">REGISTER</button>
         </div>
-      
-        {{-- <div class="signin">
-          <p>Already have an account? <a href="login">Sign in</a>.</p>
-        </div> --}}
-    
       </form>
-
       <img src="https://th.bing.com/th/id/R.58d5d1ea372f46397041d43bfbc52d48?rik=d2AxDLCa8E945w&riu=http%3a%2f%2f4.bp.blogspot.com%2f-qhglf124V_8%2fUQpvkdxXNRI%2fAAAAAAAABXI%2f0JUyFAhi6kM%2fs1600%2femployee-Management-System.jpg&ehk=Mp%2bbF1NMC4UAc2o%2bBnzZSeVIDrUPjydLVubc5UPFw%2bQ%3d&risl=&pid=ImgRaw&r=0" alt=" "  width="1000" height="280">
-
 </body>
 </html>
