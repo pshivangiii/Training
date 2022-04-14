cb0d6f4a03a5d3e0296e8fe6e4ad7c01e3371aad
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-04-14 12:20:36 +0530

    Add files via upload

--- views/logout.blade.php
+++ views/logout.blade.php
@@ -9,15 +9,10 @@
     <link rel="stylesheet" type="text/css" href="login.css">
 </head>
 <body>
-    {{-- <h1> LOGOUT </h1> --}}
     <form class ="row g-3" action="{{url('/')}}/logout" method="POST">
         {{ csrf_field() }}
-       
           <H3>ARE YOU SURE YOU WANT TO LOGOUT?</H3>
           <button type="submit"> LOGOUT </button>
-          
-          {{-- <button type="submit"> REMAIN LOGGED IN </button> --}}
-          {{-- <button type="submit"><a class="nav-link" href="/dash">REMAIN LOGGED IN</a></button> --}}
        </form>
 </body>
 </html> 
