40bd7870252dd534eb1533f54732d6f827268505
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-04-21 00:42:06 +0530

    Add files via upload

--- views/afterlogin.blade.php
+++ views/afterlogin.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+ <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -8,20 +8,18 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    
-</head>
-<body>
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Employee Management</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+ </head>
+ <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Employee Management</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-       @foreach($users as $key => $users)
+     <ul class="navbar-nav mr-auto">
+    @foreach($users as $key => $users)
       <li class="nav-item">
         <a class="nav-link" href='/payroll_details/{{ $users->email}}'>Payroll Details</a>
       </li>
@@ -32,26 +30,25 @@
         <a class="nav-link" href='/ownprofile/{{$users->email}}'>Profile Details</a>
       </li>
      
-      @if($users->designation == 'Manager')
+    @if($users->designation == 'Manager')
       <li class="nav-item">
         <a class="nav-link" href='/team_details/{{$users->team}}'>Team Details</a>
       </li>
-      @endif
-      @endforeach
+    @endif
+    @endforeach
       <li class="nav-item">
         <a class="nav-link" href="/logout"><b>Logout</b></a>
       </li>
-    </ul>
-  </div>
-  
-</nav>
-<style>
+     </ul>
+   </div> 
+ </nav>
+  <style>
     h1 {text-align: center;}
     p {text-align: center;}
     div {text-align: center;}
-    </style>
-<img src="https://wallpaperaccess.com/full/5137786.jpg" alt=" "  width="1280" height="488">
-</body>
+  </style>
+ <img src="https://wallpaperaccess.com/full/5137786.jpg" alt=" "  width="1280" height="488">
+ </body>
 </html>
 
 
