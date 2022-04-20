f216b0771cab48821293f09803ac1b5a6fcdeed6
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-04-20 09:59:12 +0530

    Changes in EmployeeController

--- Controllers/EmployeeController.php
+++ Controllers/EmployeeController.php
@@ -13,43 +13,72 @@ class EmployeeController extends Controller
 //EDIT EMPLOYEE DETAILS
     public function index()
     {
-        $users=EmployeeDetails::allData();
-        return view('updateEmp',['users'=>$users]);
+        try
+        {
+            $users=EmployeeDetails::allData();
+            return view('updateEmp',['users'=>$users]);
+        }
+        catch (\Exception $e) 
+        {
+            return redirect('error')->with
+            (
+               'error', $e->getMessage()
+            );
+        }
     }
 
     public function show(Request $request,$email) 
     {
-        $value = $request->session()->get('email');
-        if(empty($value))
-        { 
-            return view('newAdminLogin');
+        try
+        {
+            $users=EmployeeDetails::specificData($email);
+            return view('update',['users'=>$users]);
         }
-        $users=EmployeeDetails::specificData($email);
-        return view('update',['users'=>$users]);
+        catch (\Exception $e) 
+        {
+            return redirect('error')->with
+            (
+               'error', $e->getMessage()
+            );
+        }    
     }
 
     public function edit(Request $request,$id)
     {
-        
-        $email = $request->input('email');
-        $password = $request->input('psw');
-        $team = $request->input('team');
-        $designation = $request->input('designation');
-        EmployeeDetails::updateProfile($id,$email,$password,$team,$designation);
-        echo "Record updated successfully.";
+        try
+        {
+            $email = $request->input('email');
+            $password = $request->input('psw');
+            $team = $request->input('team');
+            $designation = $request->input('designation');
+            EmployeeDetails::updateProfile($id,$email,$password,$team,$designation);
+            echo "Record updated successfully.";
+        }
+        catch (\Exception $e) 
+        {
+            return redirect('error')->with
+            (
+               'error', $e->getMessage()
+            );
+        }
     }
+
     //VIEW TEAM'S DATA IF YOU'RE A MANAGER
     public function myTeam(Request $request,$team)
     {
-        $value = $request->session()->get('email');
-      if(empty($value))
-      { 
-          return view('newLogin');
-      }
-        $users=EmployeeDetails::viewTeam($team);
-        return view('myteam',['users'=>$users]); 
+        try
+        {
+            $users=EmployeeDetails::viewTeam($team);
+            return view('myteam',['users'=>$users]); 
+        }
+        catch (\Exception $e) 
+        {
+            return redirect('error')->with
+            (
+               'error', $e->getMessage()
+            );
+        }    
     }
-
 }
 
 
