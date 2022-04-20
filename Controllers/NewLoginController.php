13ee48f0210932a5ec7b74ecf33845b672834007
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-04-20 23:45:28 +0530

    Add files via upload

--- Controllers/NewLoginController.php
+++ Controllers/NewLoginController.php
@@ -13,65 +13,99 @@ class NewLoginController extends Controller
 {
     public function getLogin(Request $request)
     {
-    
-        $value = $request->session()->get('email');
-        if(!empty($value))
+        try
         {
-            $users=EmployeeDetails::getLogin($value,$request);
-            return view('afterlogin')->with($users);
-        
+            $value = $request->session()->get('email');
+            if(!empty($value))
+            { 
+                $users=EmployeeDetails::getLogin($value,$request);
+                return view('afterlogin')->with($users);
+            }
+            return view('newlogin');
         }
-        return view('newlogin');
+        catch (\Exception $e) 
+        {
+            return redirect('error')->with
+            (
+           'error', $e->getMessage()
+            );
+        }
     }
-    
-
     public function postLogin(Request $request)
     {
-        $email=$request->input('email');
-
-        $password=$request->input('password');
-
-        $users=EmployeeDetails::login($email,$password,$request);
-
-        if(isset($users))
+        try
         {
+            $email=$request->input('email');
+
+            $password=$request->input('password');
+
+            $users=EmployeeDetails::login($email,$password,$request);
+
+            if(isset($users))
+            {
           
-          return view('afterlogin')->with($users);
+                return view('afterlogin')->with($users);
+            }
+            else
+            {
+                return redirect('newlogin');
+            }
         }
-        else
+        catch (\Exception $e) 
         {
-          return redirect('newlogin');
+            return redirect('error')->with
+            (
+               'error', $e->getMessage()
+            );
+        }   
     }
-}
 
     public function getAdminLogin(Request $request)
     {
-       
-        $value = $request->session()->get('email');
-        if(!empty($value))
-        { 
-            $users=AdminDetails::getLogin($value,$request);
-            return view('features')->with($users);
-        
+        try
+        {
+            $value = $request->session()->get('email');
+            if(!empty($value))
+            { 
+                $users=AdminDetails::getLogin($value,$request);
+                return view('features')->with($users);
+            }   
+         return view('newAdminLogin');
+        }  
+        catch (\Exception $e) 
+        {
+            return redirect('error')->with
+            (
+               'error', $e->getMessage()
+            );
         }
-        return view('newAdminLogin');
     }
 
     public function postAdminLogin(Request $request)
     {
-        $email=$request->input('email');
-
-        $password=$request->input('password');
-
-        $users=AdminDetails::login($email,$password,$request);
-
-        if(isset($users))
+        try
         {
-          return view('features')->with($users);
+            $email=$request->input('email');
+
+            $password=$request->input('password');
+
+            $users=AdminDetails::login($email,$password,$request);
+
+            if(isset($users))
+            {
+                return view('features')->with($users);
+            }
+            else
+            {
+                return view('newAdminLogin');
+            }
         }
-        else
+        catch (\Exception $e) 
         {
-          return view('newAdminLogin');
+            return redirect('error')->with
+            (
+               'error', $e->getMessage()
+            );
         }
     }
   
