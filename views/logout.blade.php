76c66ad558bd12ae04b7505941f3644f1bbd5b70
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-04-21 10:41:07 +0530

    Add files via upload

--- views/logout.blade.php
+++ views/logout.blade.php
@@ -1,18 +1,17 @@
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LOGOUT</title>
-    <link rel="stylesheet" type="text/css" href="login.css">
-</head>
-<body>
-    <form class ="row g-3" action="{{url('/')}}/logout" method="POST">
-        {{ csrf_field() }}
-          <H3>ARE YOU SURE YOU WANT TO LOGOUT?</H3>
-          <button type="submit"> LOGOUT </button>
-       </form>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>LOGOUT</title>
+        <link rel="stylesheet" type="text/css" href="login.css">
+    </head>
+    <body>
+        <form class ="row g-3" action="{{url('/')}}/logout" method="POST">
+            {{ csrf_field() }}
+            <H3>ARE YOU SURE YOU WANT TO LOGOUT?</H3>
+            <button type="submit"> LOGOUT </button>
+        </form>
+    </body>
 </html> 
