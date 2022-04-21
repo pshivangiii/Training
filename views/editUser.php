bc305b6ec80bb823581e7cb9dcdcb6e41f2941d9
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-04-21 10:21:18 +0530

    Add files via upload

--- views/editUser.php
+++ views/editUser.php
@@ -1,38 +1,38 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Student Management | Edit</title>
-</head>
-<body>
-<form action = "/edit/<?php echo $data[0]->id; ?>" method = "post">
-<input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
-<table>
-<tr>
-<td>Email</td>
-<td>
-<input type = 'text' name = 'first_name'
-value = '<?php echo$data[0]->email; ?>'/> </td>
-</tr>
-<tr>
-<td>Team</td>
-<td>
-<input type = 'text' name = 'last_name'
-value = '<?php echo$data[0]->teams; ?>'/>
-</td>
-</tr>
-<tr>
-<td>Designation</td>
-<td>
-<input type = 'text' name = 'city_name'
-value = '<?php echo$data[0]->designation; ?>'/>
-</td>
-</tr>
-<tr>
-<td colspan = '2'>
-<input type = 'submit' value = "Update student" />
-</td>
-</tr>
-</table>
-</form>
-</body>
+ <head>
+ <title>Student Management | Edit</title>
+ </head>
+ <body>
+    <form action = "/edit/<?php echo $data[0]->id; ?>" method = "post">
+    <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
+        <table>
+            <tr>
+                <td>Email</td>
+                <td>
+                <input type = 'text' name = 'first_name'
+                value = '<?php echo$data[0]->email; ?>'/> </td>
+            </tr>
+            <tr>
+                <td>Team</td>
+                <td>
+                <input type = 'text' name = 'last_name'
+                value = '<?php echo$data[0]->teams; ?>'/>
+                </td>
+            </tr>
+            <tr>
+                <td>Designation</td>
+                <td>
+                <input type = 'text' name = 'city_name'
+                value = '<?php echo$data[0]->designation; ?>'/>
+                </td>
+            </tr>
+            <tr>
+                <td colspan = '2'>
+                <input type = 'submit' value = "Update student" />
+                </td>
+            </tr>
+        </table>
+    </form>
+ </body>
 </html>
