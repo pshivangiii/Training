8cf869da47a489e0eaf02bb017e7d1cfed1a3836
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-04-21 11:50:03 +0530

    Add files via upload

--- views/newreg.blade.php
+++ views/newreg.blade.php
@@ -1,29 +1,26 @@
 <!--REGISTRATION PAGE -->
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>REGISTRATION</title>
-    <link rel = "stylesheet" href = "reg.css">
-</head>
-<body style="text-align:center;">  
-
+  </head>
+  <body style="text-align:center;">  
     <h1> REGISTRATION FORM </h1>
     <form class ="row g-3" action="{{url('/')}}/newreg" method="POST">
-      
-      {{ csrf_field() }}
+       {{ csrf_field() }}
         <div class="container">
           <label for="email"><b>Email</b></label>
-          <input type="text" pattern="[^ @]*@[^ @]*" placeholder="Enter Email" name="email" id="email" >
+          <input type="text"  placeholder="Enter Email" name="email" id="email" >
          <div>
          @php
           foreach ($errors->get('email') as $message) {
             echo $message;
            }
-           @endphp
-          </div>
+         @endphp
+         </div>
           <br>
           <label for=""><b>Team</b></label>
           <select name="team" id="team">
@@ -45,7 +42,7 @@
           <br><br>
          <div>
          @php
-          foreach ($errors->get('email') as $message) {
+          foreach ($errors->get('psw') as $message) {
             echo $message;
            }
            @endphp
@@ -56,7 +53,7 @@
           <div>
            
            @php
-            foreach ($errors->get('psw') as $message) {
+            foreach ($errors->get('password') as $message) {
               echo $message;
              }
              @endphp 
@@ -74,7 +71,7 @@
           <br>
           <button type="submit" class="registerbtn">REGISTER</button>
         </div>
-      </form>
+    </form>
       <img src="https://th.bing.com/th/id/R.58d5d1ea372f46397041d43bfbc52d48?rik=d2AxDLCa8E945w&riu=http%3a%2f%2f4.bp.blogspot.com%2f-qhglf124V_8%2fUQpvkdxXNRI%2fAAAAAAAABXI%2f0JUyFAhi6kM%2fs1600%2femployee-Management-System.jpg&ehk=Mp%2bbF1NMC4UAc2o%2bBnzZSeVIDrUPjydLVubc5UPFw%2bQ%3d&risl=&pid=ImgRaw&r=0" alt=" "  width="1000" height="280">
-</body>
+  </body>
 </html>
